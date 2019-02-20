5d9afd0dc2a440da719aa455fa90be76955d2e95
zhoubo <zhoub1001@126.com>
2019-02-20 10:39:42 +0800

    bo
    add dio http →

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,13 +1,24 @@
 import 'package:flutter/material.dart';
-
+import 'package:dio/dio.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var data = getHttp();
     return Scaffold(
        body: Center(
          child: Text('首页'),
        ),
     );
   }
-}
+
+  getHttp() async {
+    try{
+      Response response;
+      response = await Dio().get('https://www.easy-mock.com/mock/5c60131a4bed3a6342711498/baixing/dabaojian?name=abc');
+      return print(response);
+    }catch(e){
+      print(e);
+    }
+  }
+ }
 
